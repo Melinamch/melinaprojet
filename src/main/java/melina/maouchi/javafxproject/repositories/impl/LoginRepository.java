652e1270021c5f8a6166297762d2a3d72007917d
Melinamch <melinamaouchi11@gmail.com>
2025-02-10 15:15:39 +0100

    login

---
+++ src/main/java/melina/maouchi/javafxproject/repositories/impl/LoginRepository.java
@@ -0,0 +1,34 @@
+package melina.maouchi.javafxproject.repositories.impl;
+
+
+
+import melina.maouchi.javafxproject.config.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginRepository {
+
+    public String authenticateUser(String username, String password) {
+        String role = null;
+        String sql = "SELECT role FROM users WHERE name = ? AND password = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                role = rs.getString("role");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return role;
+    }
+}
+
